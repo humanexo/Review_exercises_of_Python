8282e39dc401fa39ad379b53a3188ba7ae115952
Andrés Felipe <andresdediaz10@gmail.com>
2023-02-09 22:36:55 -0500

    Exercise done!

---
+++ ejercicio_cuatro.py
@@ -0,0 +1,12 @@
+
+if __name__ == "__main__":
+
+    number = int(input("Enter your number: "))
+
+
+    if number % 2 == 0:
+        print(f"The number {number} is even")
+
+
+    else:
+        print(f"The number {number} is uneven")
