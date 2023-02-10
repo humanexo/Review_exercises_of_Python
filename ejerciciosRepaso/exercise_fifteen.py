8126d875a0438cd2092578f9006db17282f01d2b
Andrés Felipe <andresdediaz10@gmail.com>
2023-02-10 08:20:21 -0500

    Exercise done! And rename files.

---
+++ ejerciciosRepaso/exercise_fifteen.py
@@ -0,0 +1,17 @@
+
+if __name__ == "__main__":
+
+
+    word = input ("Enter your word: ")
+
+
+    if str (word) == str (word) [::-1]:
+
+
+        print (f"The word {word} is Palindrome")
+
+
+    else:
+
+
+        print (f"The word {word} is not Palindrome")
