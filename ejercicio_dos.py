8018ce77de18fd75c3a83f06b91ba1d517a831ae
Andrés Felipe <andresdediaz10@gmail.com>
2023-02-09 22:23:50 -0500

    Exercise done!

---
+++ ejercicio_dos.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+
+    def areaOfACircle():
+        radius = int(input("Enter the radius of your circle: "))
+
+
+        area = 3.1416 * radius ** 2
+        print(f"The area is {area}")
+
+    areaOfACircle()
+
